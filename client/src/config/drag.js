b535297d781422968b2504f6ae383fc72d61002b
张健 <545300506@qq.com>
2018-08-14 15:31:59 +0800

    Modify drag

--- client/src/config/drag.js
+++ client/src/config/drag.js
@@ -8,6 +8,28 @@ class Drag {
       this.target = document.getElementById(target)
       this.ondragstart()
       this.ondrag()
+      this.onmousedown()
+    }
+    onmousedown () {
+      this.target.addEventListener('mousedown', (e) => {
+        // var scrollX = document.body.scrollLeft;
+        // var scrollY = document.body.scrollTop;
+        // var x = e.clientX + scrollX;
+        // var y = e.clientY + scrollY;
+        let clientX = e.clientX  // 鼠标点击坐标距离浏览器的x坐标
+        let clientY = e.clientY
+        let screenX = window.screenX // 浏览器左边缘距离屏幕视窗的x坐标
+        let screenY = window.screenY
+        // console.log(screenX, screenY);
+        console.log(clientX, clientY);
+        // return { x : x, y : y };
+      })
+    }
+    onmouseup () {
+
+    }
+    onmousemove () {
+
     }
     ondragstart () {
       this.target.addEventListener('dragstart', () => {
